9417b62c3508822ccf344e44dccd8f846d8358f1
Raúl Sosa <RaulSG188@gmail.com>
2025-07-30 17:33:39 +0200

    Half project

--- Demographic data analyzer/demographic_data_analyzer.py
+++ Demographic data analyzer/demographic_data_analyzer.py
@@ -26,12 +26,13 @@ def calculate_demographic_data(print_data=True):
     lower_education_rich = (df[~df["education"].isin(["Bachelors", "Masters", "Doctorate"])]["salary"] == ">50K").mean() * 100
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = df["hours-per-week"].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
+    num_min_workers = (df[df["hours-per-week"] == min_work_hours]["salary"] == ">50K").shape[0]
 
-    rich_percentage = None
+
+    rich_percentage = (100 * num_min_workers) / df.shape[0]
 
     # What country has the highest percentage of people that earn >50K?
     highest_earning_country = None
